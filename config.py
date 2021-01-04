c9b25055d17f12ed4ea19fdc9666d52bf9a1f364
godestalbin <godestalbin@gmail.com>
2021-01-04 18:33:32 +0100

    Disabled CSRF

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    WTF_CSRF_ENABLED = False
 
     # Mongo keys defined at https://cloud.mongodb.com/v2/5fc2042fdc81aa5cf552a7af#clusters
     MONGO_DB = 'mongodb+srv://godestalbin:YK8SfqWUleMufpYz@cluster0.dyrcm.mongodb.net/?retryWrites=true&w=majority'
