4891cb4227d2b32230e6ac2aabee4e66b045d6cf
godestalbin <godestalbin@gmail.com>
2021-01-10 10:48:25 +0100

    SMS corrected

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
         self.MONGO_DB = Config.accessSecret(self, self.GCLOUD_PROJECT_ID, 'MONGO_DB')
         self.DATABASE = Config.accessSecret(self, self.GCLOUD_PROJECT_ID, 'DATABASE')
         self.COLLECTION = Config.accessSecret(self, self.GCLOUD_PROJECT_ID, 'COLLECTION')
-        self.FREE_MOBILE_SMS_GATEWAY = Config.accessSecret(self, self.GCLOUD_PROJECT_ID, 'FREE_MOBILE_SMS_GATEWAY')
+        self.FREE_MOBILE_SMS_GATEWAY = [Config.accessSecret(self, self.GCLOUD_PROJECT_ID, 'FREE_MOBILE_SMS_GATEWAY')]
 
     def accessSecret(self, project_id, secret_id, version='latest'):
         client = secretmanager.SecretManagerServiceClient()
